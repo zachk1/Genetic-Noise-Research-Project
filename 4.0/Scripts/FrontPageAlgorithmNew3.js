3a814ca9b2834df5dc70def4591132bffe43e9ef
Nabdeep Shrestha <nabdeep@vt.edu>
2019-02-26 09:46:03 -0500

    First commit

---
+++ 4.0/Scripts/FrontPageAlgorithmNew3.js
@@ -0,0 +1,235 @@
+"use strict";
+
+var ctx1 = document.getElementById('myChart1').getContext('2d');
+var chart1 = new Chart(ctx1, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Protein',
+            borderColor: '#00e68a',
+            backgroundColor: '#b3ffe0',
+            data: [{
+                x: 0,
+                y: 0
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Dimensionless Time'
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Protein Concentration'
+                }
+            }]
+        },
+        title: {
+            display: true,
+            text: 'Biomolecule Concentration'
+        },
+    }
+});
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+var chart2 = new Chart(ctx2, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'RNA',
+            borderColor: '#cc0052',
+            backgroundColor: '#ffcce0',
+            data: [{
+                x: 0,
+                y: 0
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom',
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Dimensionless Time'
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'RNA Concentration'
+                }
+            }]
+        },
+        title: {
+            display: true,
+            text: 'Biomolecule Concentration'
+        },
+    }
+});
+            
+//displays user input value
+var v1 = document.getElementById("i1");
+var o1 = document.getElementById("d1");
+o1.innerHTML = v1.value;
+v1.oninput = function () {
+    o1.innerHTML = this.value;
+};
+                
+var v2 = document.getElementById("i2");
+var o2 = document.getElementById("d2");
+o2.innerHTML = v2.value;
+v2.oninput = function () {
+    o2.innerHTML = this.value;
+};
+                
+var v3 = document.getElementById("i3");
+var o3 = document.getElementById("d3");
+o3.innerHTML = v3.value;
+v3.oninput = function () {
+    o3.innerHTML = this.value;
+};
+                
+var v4 = document.getElementById("i4");
+var o4 = document.getElementById("d4");
+o4.innerHTML = v4.value;
+v4.oninput = function () {
+    o4.innerHTML = this.value;
+};
+
+//shuffles array
+function shuffleArr(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+         // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+var timerId;
+var i = 0;
+            
+function gillespie() {
+    var transcript, translat, rnaDeg, proteinDeg, pythonArr, randPythonArr, expArr, randExpArr, randExpInt, randInt, rnaConc, proteinConc, timeValue, transcriptProb, translatProb, rnaDegProb, proteinDegProb, proteinDegProbAlpha, rnaDegProbAlpha, translatProbAlpha;
+    //gets values from html sliders and converts to numbers
+    function getValue() {
+        transcript = document.getElementById("i1");
+        transcript = transcript.value;
+        transcript = parseInt(transcript, 10);
+        translat = document.getElementById("i2");
+        translat = translat.value;
+        translat = parseInt(translat, 10);
+        rnaDeg = document.getElementById("i3");
+        rnaDeg = rnaDeg.value;
+        rnaDeg = parseInt(rnaDeg, 10);
+        proteinDeg = document.getElementById("i4");
+        proteinDeg = proteinDeg.value;
+        proteinDeg = parseInt(proteinDeg, 10);
+    }
+    
+    //array of rand num from python, exponentional distribution array from python
+    pythonArr = [69, 42, 46, 96, 73, 17, 91, 52, 95, 14, 11, 13, 81, 76, 2, 65, 84, 25, 3, 83, 70, 79, 77, 30, 56, 45, 99, 34, 87, 63, 38, 62, 19, 64, 55, 29, 20, 41, 12, 33, 88, 67, 75, 15, 50, 43, 71, 31, 24, 26, 36, 60, 78, 100, 98, 10, 66, 61, 27, 35, 40, 39, 97, 53, 9, 89, 5, 51, 58, 23, 37, 7, 8, 86, 21, 93, 49, 57, 16, 22, 82, 44, 1, 48, 85, 74, 90, 6, 94, 28, 92, 68, 59, 4, 18, 72, 47, 32, 54, 80];
+    expArr = [40, 41, 42, 43, 44, 44, 44, 44, 44, 44, 44, 44, 44, 44, 45, 45, 45, 45, 45, 45, 45, 45, 45, 45, 46, 46, 46, 46, 46, 46, 46, 46, 46, 46, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 51, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 52, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 53, 54, 54, 54, 54, 54, 54, 54, 54, 54, 54, 55, 55, 55, 55, 55, 55, 55, 55, 55, 55, 56, 56, 56, 56, 56, 56, 56, 56, 56, 56, 57, 58, 59, 60];
+    
+    //initial concentrations of reactants
+    rnaConc = 0;
+    proteinConc = 0;
+    
+    //calculates time until next rxn
+    getValue();
+    randExpArr = shuffleArr(expArr);
+    randExpInt = randExpArr[0];
+    timeValue = 1500 - (rnaConc / 12 * 500) - (proteinConc / 120000 * 500) + ((randExpInt - 50) * 10);
+
+    //Actual repeating steps of algorithm
+    timerId = setTimeout(function tick() {
+        //calculate time to next rxn
+        randExpArr = shuffleArr(expArr);
+        randExpInt = randExpArr[0];
+        timeValue = 1500 - (rnaConc / 12 * 500) - (proteinConc / 120000 * 500) + ((randExpInt - 50) * 10);
+        
+        //choose what the next rxn will be
+        transcriptProb = transcript - (transcript * (rnaConc / 12));
+        translatProb = (translat - (translat * (proteinConc / 120000))) * (rnaConc / 12);
+        rnaDegProb = (rnaDeg / 100) * rnaConc;
+        proteinDegProb = (proteinDeg / 100) * proteinConc;
+        
+        randPythonArr = shuffleArr(pythonArr);
+        randInt = randPythonArr[0] / 100 * (transcriptProb + translatProb + rnaDegProb + proteinDegProb);
+
+        translatProbAlpha = transcriptProb + translatProb;
+        alert(translatProbAlpha);
+        rnaDegProbAlpha = translatProbAlpha + rnaDegProb;
+        alert(rnaDegProbAlpha);
+        proteinDegProbAlpha = rnaDegProbAlpha + proteinDegProb;
+        alert(proteinDegProbAlpha);
+        
+        //carry out next rxn
+        if (randInt >= proteinDegProbAlpha) {
+            proteinConc = proteinConc - 1;
+        } else if (randInt >= rnaDegProbAlpha) {
+            rnaConc = rnaConc - 1;
+        } else if (randInt >= translatProbAlpha) {
+            proteinConc = proteinConc + 1;
+        } else {
+            rnaConc = rnaConc + 1;
+        }
+        
+        //updates text values for concentrations
+        document.getElementById("d5").innerHTML = rnaConc;
+        document.getElementById("d6").innerHTML = proteinConc;
+        
+        //changes graph
+        i = i + 1;
+        chart1.data.datasets.forEach((dataset) => {
+            dataset.data.push({
+                            x: i,
+                            y: proteinConc
+                        });
+        });
+        if (i > 10) {
+            chart1.data.datasets.forEach((dataset) => {
+                dataset.data.shift();
+            });
+        };
+        chart1.update();
+        
+        chart2.data.datasets.forEach((dataset) => {
+            dataset.data.push({
+                            x: i,
+                            y: rnaConc
+                        });
+        });
+        if (i > 10) {
+            chart2.data.datasets.forEach((dataset) => {
+                dataset.data.shift();
+            });
+        };
+        chart2.update();
+        
+        
+        timerId = setTimeout(tick, timeValue);
+        
+    }, timeValue);
+}
+
+function stopAlgorithm() {
+    clearTimeout(timerId);
+}
